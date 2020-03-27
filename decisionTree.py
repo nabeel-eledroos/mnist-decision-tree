c3d72aebcac197353e43a572fda28d37c085471b
Nabeel Eledroos <nabeel@eledroos.net>
2020-03-27 14:37:14 -0400

    more logic for single tree function

--- decisionTree.py
+++ decisionTree.py
@@ -50,16 +50,28 @@ def scoreFeatures(x, y):
                     pred[i,j] = 8
     return score, pred
 
-# def applyDT(points, pred_vals, depth, x, y, predictions):
-#     if depth = 1:
+def split(points, pred_vals, pred_opps, depth, x, y, predictions):
+    i = points[0][1]
+    j = points[0][0]
+    for n in range(0,200):
+        if x[i,j,n] == 1:
+            if depth>1:
+                split(points[1:], pred_vals, pred_opps, depth-1, x, y, predictions)
+            else:
+                predictions[n] = pred_vals[points[0]]
+        else:
+            if depth>1:
+                split(points[2:], pred_vals, pred_opps, depth-1, x, y, predictions)
+            else:
+                predictions[n] = pred_opps[points[0]]
+    return predictions
 
-#         return predictions
-#     i = points[0][1]
-#     j = points[0][0]
-#     for n in range(0,200):
-#         if x[i,j,n] == 1:
-
-# def split():
+def applyDT(points, pred_vals, depth, x, y):
+    predictions = np.zeros(200)
+    pred_opps = {}
+    for p in points:
+        pred_opps[p] = ((pred_vals[p]-5)%10)
+    return split(points, pred_vals, pred_opps, depth, x, y, predictions)
 
 
 def applyDT1(p, pred, x, y):
@@ -97,37 +109,40 @@ if __name__ == '__main__':
     score1,pred1 = scoreFeatures(x_train, y_train)
     # plt.imshow(score1, cmap='gray')
     # plt.show()
-
+    pred_vals = {}
+    points = []
     a = np.max(score1)
-    print(a)
     (i,j) = (np.where(score1 == a)[1][0], (np.where(score1 == a)[0][0]))
     p1 = (i,j)
+    pred_vals[p1] = pred1[j, i]
+    points.append(p1)
     print(p1)
 
-    testDepth1 = applyDT1((i,j), pred1[j, i], x_test, y_test)
-    print(testDepth1)
+    testDepth1 = applyDT(points, pred_vals, 1, x_test, y_test)#applyDT1((i,j), pred1[j, i], x_test, y_test)
+    print(accuracy(y_test, testDepth1))
 
-    ind1 = np.where(x_train[j,i,:]==1)
-    ind2 = np.where(x_train[j,i,:]==0)
 
-    sub1x = x_train[:,:,ind1[0]]
-    sub1y = y_train[ind1[0]]
+    # ind1 = np.where(x_train[j,i,:]==1)
+    # ind2 = np.where(x_train[j,i,:]==0)
 
-    sub2x = x_train[:,:,ind2[0]]
-    sub2y = y_train[ind2[0]]
+    # sub1x = x_train[:,:,ind1[0]]
+    # sub1y = y_train[ind1[0]]
 
-    score2,pred2 = scoreFeatures(sub1x,sub1y)
+    # sub2x = x_train[:,:,ind2[0]]
+    # sub2y = y_train[ind2[0]]
 
-    b = np.max(score2)
-    (k,l) = (np.where(score2 == b)[1][0], (np.where(score2 == b)[0][0]))
-    p2 = (k,l)
-    print(p2)
+    # score2,pred2 = scoreFeatures(sub1x,sub1y)
 
-    score3,pred3 = scoreFeatures(sub2x,sub2y)
-    c = np.max(score3)
-    (m,n) = (np.where(score3 == c)[1][0], (np.where(score3 == c)[0][0]))
-    p3 = (m,n)
-    print(p3)
+    # b = np.max(score2)
+    # (k,l) = (np.where(score2 == b)[1][0], (np.where(score2 == b)[0][0]))
+    # p2 = (k,l)
+    # print(p2)
 
-    testDepth2 = applyDT2(p1, p2, pred2[l, k], p3, pred3[n, m], x_test, y_test)
-    print(testDepth2)
+    # score3,pred3 = scoreFeatures(sub2x,sub2y)
+    # c = np.max(score3)
+    # (m,n) = (np.where(score3 == c)[1][0], (np.where(score3 == c)[0][0]))
+    # p3 = (m,n)
+    # print(p3)
+
+    # testDepth2 = applyDT2(p1, p2, pred2[l, k], p3, pred3[n, m], x_test, y_test)
+    # print(testDepth2)
