8b871e08fda33156f1009cfd5cee2964f3c24b43
Nabeel Eledroos <nabeel@eledroos.net>
2020-03-28 14:12:03 -0400

    added 'fit' function for training data

--- decisionTree.py
+++ decisionTree.py
@@ -2,8 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class decisionTree:
+    pred_vals = {}
+    pred_opps = {}
+    points = []
+    depth = 0
+
     def __init__(self, depth=1):
         self.depth = depth
+        self.predictions = np.zeros(200)
 
     def scoreFeatures(self, x, y):
         score = np.zeros((28,28)).astype(int)
@@ -43,7 +49,15 @@ class decisionTree:
         (i,j) = (np.where(score == m)[1][0], (np.where(score == m)[0][0]))
         return (i, j)
 
-    # def get_points():
+    def fit(self, x, y):
+        self.root_score, self.root_pred = self.scoreFeatures(x, y)
+        (i, j) = self.get_point(self.root_score)
+        self.pred_vals[(i, j)] = self.root_pred[j, i]
+        self.points.append((i, j))
+        # if depth > 1:
+
+        #     for d in range(self.depth):
+
 
 
     def split(self, n, points, pred_vals, pred_opps, depth, x, y, predictions):
@@ -52,38 +66,19 @@ class decisionTree:
         prediction = 0
         if x[i,j,n] == 1:
             if depth>1:
-                prediction = self.split(n, points[1:], pred_vals, pred_opps, depth-1, x, y, predictions)
+                prediction = self.split(n, points[1:], depth-1, x, y, predictions)
             else:
-                prediction = pred_vals[points[0]]
+                prediction = self.pred_vals[points[0]]
         else:
             if depth>1:
-                prediction = self.split(n, points[2:], pred_vals, pred_opps, depth-1, x, y, predictions)
+                prediction = self.split(n, points[2:], depth-1, x, y, predictions)
             else:
-                prediction = pred_opps[points[0]]
+                prediction = self.pred_opps[points[0]]
         return prediction
 
-    def applyDT(self, points, pred_vals, depth, x, y):
-        predictions = np.zeros(200)
-        pred_opps = {}
-        for p in points:
-            pred_opps[p] = ((pred_vals[p]-5)%10)
+    def predict(self, x, y):
+        for p in self.points:
+            self.pred_opps[p] = ((self.pred_vals[p]-5)%10)
         for n in range(200):
-            predictions[n] = self.split(n, points, pred_vals, pred_opps, depth, x, y, predictions)
-        return predictions
-
-    # def applyDT2(self, p1, p2, pred2, p3, pred3, x, y):
-    #     predictions = np.zeros(200)
-    #     pred2_opp = (pred2-5)%10
-    #     pred3_opp = (pred3-5)%10
-    #     for n in range(0,200):
-    #         if x[p1[1],p1[0],n] == 1:
-    #             if x[p2[1],p2[0],n] == 1:
-    #                 predictions[n] = pred2
-    #             else:
-    #                 predictions[n] = pred2_opp
-    #         else:
-    #             if x[p3[1],p3[0],n] == 1:
-    #                 predictions[n] = pred3
-    #             else:
-    #                 predictions[n] = pred3_opp
-    #     return predictions
+            self.predictions[n] = self.split(n, self.points, self.pred_vals, self.pred_opps, self.depth, x, y, self.predictions)
+        return self.predictions
--- run.py
+++ run.py
@@ -19,50 +19,38 @@ def loadmat(path):
 #Function test
 if __name__ == '__main__':
     x_train, y_train, x_test, y_test = loadmat('data.mat')
-    tree = decisionTree()
-    pred_vals = {}
-    points = []
 
-    score1,pred1 = tree.scoreFeatures(x_train, y_train)
-
-    (i,j) = tree.get_point(score1)
-    p1 = (i,j)
-    pred_vals[p1] = pred1[j, i]
-    points.append(p1)
-    print(p1)
-
-    testDepth1 = tree.applyDT(points, pred_vals, 1, x_test, y_test)
-    print(tree.accuracy(y_test, testDepth1))
+    tree1 = decisionTree(depth=1)
+    tree1.fit(x_train, y_train)
+    testDepth1 = tree1.predict(x_test, y_test)
+    print(tree1.accuracy(y_test, testDepth1))
 
 
-    ind1 = np.where(x_train[j,i,:]==1)
-    ind2 = np.where(x_train[j,i,:]==0)
+    # ind1 = np.where(x_train[j,i,:]==1)
+    # ind2 = np.where(x_train[j,i,:]==0)
 
-    sub1x = x_train[:,:,ind1[0]]
-    sub1y = y_train[ind1[0]]
+    # sub1x = x_train[:,:,ind1[0]]
+    # sub1y = y_train[ind1[0]]
 
-    sub2x = x_train[:,:,ind2[0]]
-    sub2y = y_train[ind2[0]]
+    # sub2x = x_train[:,:,ind2[0]]
+    # sub2y = y_train[ind2[0]]
 
-    score2,pred2 = tree.scoreFeatures(sub1x,sub1y)
+    # score2,pred2 = tree.scoreFeatures(sub1x,sub1y)
 
-    (k,l) = tree.get_point(score2)
-    p2 = (k,l)
-    pred_vals[p2] = pred2[l, k]
-    points.append(p2)
-    print(p2)
+    # (k,l) = tree.get_point(score2)
+    # p2 = (k,l)
+    # pred_vals[p2] = pred2[l, k]
+    # points.append(p2)
+    # print(p2)
 
 
-    score3,pred3 = tree.scoreFeatures(sub2x,sub2y)
-    (m,n) = tree.get_point(score3)
-    p3 = (m,n)
-    pred_vals[p3] = pred3[n, m]
-    points.append(p3)
-    print(p3)
+    # score3,pred3 = tree.scoreFeatures(sub2x,sub2y)
+    # (m,n) = tree.get_point(score3)
+    # p3 = (m,n)
+    # pred_vals[p3] = pred3[n, m]
+    # points.append(p3)
+    # print(p3)
 
-    # print(points, pred_vals)
-    d2 = tree.applyDT2(p1, p2, pred2[l, k], p3, pred3[n, m], x_test, y_test)
-    testDepth2 = tree.applyDT(points, pred_vals, 2, x_test, y_test) #applyDT2(p1, p2, pred2[l, k], p3, pred3[n, m], x_test, y_test)
-    # print(d2)
-    # print(testDepth2-d2)
-    print(tree.accuracy(y_test, testDepth2))
+    # testDepth2 = tree.applyDT(points, pred_vals, 2, x_test, y_test)
+
+    # print(tree.accuracy(y_test, testDepth2))
